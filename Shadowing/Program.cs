19e9ca04e0dc41b890cf31b3938b5bc5ba7b65cc
hovhannisyan.vahagn <hovhannisyan.vahagn@B7F1HR-02>
2020-06-29 10:21:32 +0400

    Add project files.

---
+++ Shadowing/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shadowing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 
+            Sales obj3 = new Sales();
+            obj3.InvoiceNumber = 123;
+            SalesVersion2 obj4 = new SalesVersion2();
+            obj4.InvoiceNumber = "Hello";
+
+            //overriding change the implementation
+            Class1 obj1 = new Class1();
+            Class2 obj2 = new Class2();
+            // i is a variable
+            obj1.i = 123;
+
+            // i has been converted to a method
+            // parent class element is compeletly replaced by the child class //shadowing
+            obj2.i();
+
+            Console.ReadLine();
+        }
+    }
+    public class Sales
+    {
+        public int InvoiceNumber { get; set; }
+    }
+    public class SalesVersion2 : Sales
+    {
+        public object InvoiceNumber { get; set; }
+    }
+    public class Class1
+    {
+        public int i = 0;
+    }
+    public class Class2 : Class1
+    {
+        public void i()
+        {
+
+        }
+    }
+}
---
+++ YieldKeyword/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldKeyword
+{
+    class Program
+    {
+         static List<int> MyList = new List<int>();
+        static void FillValues()
+        {
+            MyList.Add(1);
+            MyList.Add(2);
+            MyList.Add(3);
+            MyList.Add(4);
+            MyList.Add(5);
+            MyList.Add(6);
+
+        }
+        static IEnumerable<int> Filter()
+        {
+            List<int> temp = new List<int>();
+            foreach (var item in MyList)
+            {
+                if (item>3)
+                {
+                    temp.Add(item);
+                }
+            }
+            return temp;
+        }
+        //Filtering without of a temp collection
+        static IEnumerable<int> FilterWithYield()
+        {
+         
+            foreach (var item in MyList)
+            {
+                if (item > 3)
+                {
+                    yield return item;
+                }
+            }
+         
+        }
+        static IEnumerable<int> RunningTotal()
+        {
+            int runningTotal = 0;
+            foreach (var item in MyList)
+            {
+                runningTotal += item;
+                yield return (runningTotal);
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            //Yield helps to do custom stateful iteration over a collection 
+            FillValues();
+            foreach (var item in FilterWithYield())
+            {
+                //If condition is true he remember that has to start from wich number
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Running Total");
+            foreach (var item in RunningTotal())
+            {
+                //If condition is true he remember that has to start from wich number
+                Console.WriteLine(item);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
