52e0e8e3e0f49927fcadd622b146593b281b518f
hvahagn <vhoagamo@gmail.com>
2020-06-30 14:25:15 +0400

    add lambda

---
+++ AsyncVSThread/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncVSThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Asyncronous code does not use or create threads. Making app usable, non blocking main thread which is doing main work is non blocking.
+            // It is not improving performance and not creating new threads.
+            Thread x = new Thread(SomeMethod);
+            x.Start();
+
+            SomeMethod();
+            Console.WriteLine("This is main method");
+
+
+
+            Console.ReadLine();
+        }
+        static async void SomeMethod()
+        {
+            //for asyncronous
+           await Task.Delay(60000);
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ LambdaExpActionFuncPredicate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpActionFuncPredicate
+{
+    class Program
+    {
+       delegate double CalArepointer(int r);
+       static CalArepointer cpointer = CalculateArea;
+
+        static void Main(string[] args)
+        {
+
+            double area = cpointer.Invoke(20);
+             
+
+
+            Console.ReadLine();
+        }
+
+        static double CalculateArea(int r)
+        {
+            return 3.14 * r * r;
+        }
+    }
+}
